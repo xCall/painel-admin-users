adb86b71cd3051db732d582f3fe61c0c4e949f6b
xCall <carlosfaita99@gmail.com>
2022-02-05 10:53:31 -0300

    refatoraçao

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
-let gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
-let name = document.querySelector('#exampleInputName');
-let birth = document.querySelector('#exampleInputBirth');
-let country = document.querySelector('#exampleInputCountry');
-let email = document.querySelector('#exampleInputEmail');
-let password = document.querySelector('#exampleInputpassword');
-let photo = document.querySelector('#exampleInputFile');
-let admin = document.querySelector('#exampleInputAdmin');
+let fields = document.querySelectorAll('#form-user-create [name]');
+let user = {};
 
-
-var fields = document.querySelectorAll('#form-user-create [name]');
-
-fields.forEach((field) => {
-  if (field.name == 'gender') {
-    if (field.checked) console.log(`sim ${field}`);
-  } else {
-    console.log('nao');
-  }
+document.querySelectorAll('#form-user-create').forEach(() => {
+  this.addEventListener('submit', (event) => {
+    event.preventDefault();
+    fields.forEach((field) => {
+      if (field.name == 'gender') {
+        if (field.checked) user[field.name] = field.value;
+      } else {
+        user[field.name] = field.value;
+      }
+    });
+  });
 });
+
+console.log(user);
