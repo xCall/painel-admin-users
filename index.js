12d806b184e5cd048b1373e518914b53d20a08f0
xCall <carlosfaita99@gmail.com>
2022-02-05 00:14:10 -0300

    Estudo de seletores

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-let nome = document.querySelector('#exampleInputName');
+let gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+let name = document.querySelector('#exampleInputName');
+let birth = document.querySelector('#exampleInputBirth');
+let country = document.querySelector('#exampleInputCountry');
+let email = document.querySelector('#exampleInputEmail');
+let password = document.querySelector('#exampleInputpassword');
+let photo = document.querySelector('#exampleInputFile');
+let admin = document.querySelector('#exampleInputAdmin');
 
-console.log(nome.value = 'carlos');
+
+var fields = document.querySelectorAll('#form-user-create [name]');
+
+fields.forEach((field) => {
+  if (field.name == 'gender') {
+    if (field.checked) console.log(`sim ${field}`);
+  } else {
+    console.log('nao');
+  }
+});
