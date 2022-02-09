7e7746e660c2db0e3724b1611576895b752fb0fa
xCall <carlosfaita99@gmail.com>
2022-02-08 22:16:05 -0300

    adicionado ao repositorio

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 let fields = document.querySelectorAll('#form-user-create [name]');
 let user = {};
 
-document.querySelectorAll('#form-user-create').forEach(() => {
-  this.addEventListener('submit', (event) => {
-    event.preventDefault();
-    fields.forEach((field) => {
-      if (field.name == 'gender') {
-        if (field.checked) user[field.name] = field.value;
-      } else {
-        user[field.name] = field.value;
-      }
-    });
+function addLine(dataUser) {
+  var tr = document.createElement('tr');
+
+  document.getElementById('table-users').innerHTML =
+    `
+    <tr>
+      <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+      <td>${dataUser.name}</td>
+      <td>${dataUser.email}</td>
+      <td>${dataUser.admin}</td>
+      <td>${dataUser.birth}</td>
+      <td>
+        <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+        <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+      </td>
+    </tr>
+  `;
+}
+
+document.getElementById('form-user-create').addEventListener('submit', function (event) {
+
+  event.preventDefault();
+  fields.forEach(function (field) {
+    if (field.name == 'gender') {
+      if (field.checked) user[field.name] = field.value;
+    } else {
+      user[field.name] = field.value;
+    }
   });
+
+  let objectUser = new User(user);
+
+  addLine(user);
 });
 
-console.log(user);
---
+++ models/User.js
@@ -0,0 +1,12 @@
+class User {
+  constructor(name, gender, birth, country, email, password, photo, admin) {
+    this._name = name;
+    this._gender = gender;
+    this._birth = birth;
+    this._country = country;
+    this._email = email;
+    this._password = password;
+    this._photo = photo;
+    this._admin = admin;
+  }
+}
